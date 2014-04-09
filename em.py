1dbc0fd58b747d40052c96b6ecdf5579507d4b03
anoukv <anouk.visser@student.uva.nl>
2014-04-09 11:37:28 +0200

    init translation probs

--- em.py
+++ em.py
@@ -12,5 +12,14 @@ def loadData(f = "corpus.nl", e = "corpus.en"):
 
 	return (f, e)
 
+def initializeT(coprus):
+	translationProbs = dict()
+	
+	for sentencePair in coprus:
+		for f in sentencePair[0]:
+			for e in sentencePair[1]:
+				translationProbs[(f, e)] = 1
+	return translationProbs
+
 if __name__ == "__main__":
 	print loadData()[0][:5]
