d4fcc1fe630f106cf17ef2af2446f24ba669758e
RemideZ <remi.de.z@gmail.com>
2014-04-09 11:30:24 +0200

    hy

--- em.py
+++ em.py
@@ -1,8 +1,16 @@
 
 
 
-def loadData(filename=""):
+def loadData(f = "corpus.nl", e = "corpus.en"):
+	fileo = open(f,'r')
+	f = map(lambda x : x.replace("\n","").split(" "), fileo.readlines())
+	fileo.close()
 
+	fileo = open(e,'r')
+	e = map(lambda x : x.replace("\n","").split(" "), fileo.readlines())
+	fileo.close()
 
+	return (f, e)
 
 if __name__ == "__main__":
+	print loadData()[0][:5]
