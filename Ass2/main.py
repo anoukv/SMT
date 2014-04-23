6a97a67438ebbbb6f8d1e85b9edc14933a0be9b2
RemideZ <remi.de.z@gmail.com>
2014-04-23 12:31:23 +0200

    started working on probabilities

--- Ass2/main.py
+++ Ass2/main.py
@@ -2,6 +2,9 @@ from collections import defaultdict
 
 
 def read_sentences_from_file(filename):
+	"""
+		Reads sentences from file.
+	"""
 	f = open(filename, 'r')
 	content = f.readlines()
 	f.close()
@@ -10,6 +13,9 @@ def read_sentences_from_file(filename):
 	return filter(lambda x : not len(x) == 0, content)
 
 def extract_phrases_from_sentence(sentence, length=4):
+	"""
+		Reads grams from file and returns them in a dictionary with their counts.
+	"""
 	def add_gram_to_dic(gram, dic):
 		for i in xrange(len(gram)):
 			dic[tuple(gram[i:])] += 1
@@ -27,13 +33,31 @@ def extract_phrases_from_sentence(sentence, length=4):
 
 	return phrases
 
-def extract_all_phrases(sentences):
+def extract_all_phrases(sentence_pairs):
+	"""
+		Generates phrases for a collection of sentences.
+	"""
 	def add_phrases_to_dic(phrases, dic):
 		for key, value in phrases.items():
 			dic[key] += value
+	def add_phrases_to_coc(pf, pe, dic):
+		for (p1, p2) in [(pf,pe), (pe,pf)]:
+			for key, value in p1.items():
+				for p in p2.keys():
+					dic[p][key] += value
 
-	phrases = defaultdict(int)
-	for sentence in sentences:
-		add_phrases_to_dic(extract_phrases_from_sentence(sentence), phrases)
+	phrases_f = defaultdict(int)
+	phrases_e = defaultdict(int)
+	cocs = defaultdict(defaultdict)
+	for (sentence_f, sentence_e) in sentences:
+		p_f = extract_phrases_from_sentence(sentence_f)
+		p_e = extract_phrases_from_sentence(sentence_e)
 
-	return phrases
+		add_phrases_to_dic(p_f, phrases_f)
+		add_phrases_to_dic(p_e, phrases_e)
+		add_phrases_to_coc(p_f, p_e, phrases_e)
+
+	return (phrases_f, phrases_e, cocs)
+
+
+	
