06620d594f07fa22f2eec0ff5ecef6f9e7d73cd8
anoukv <anouk.visser@student.uva.nl>
2014-04-28 12:31:32 +0200

    coverage simple

--- Ass2/coverage.py
+++ Ass2/coverage.py
@@ -1,7 +1,7 @@
 # Computing the coverage
 
-# returns percentage of phrases in phraseTable2 that are in phraseTable1
-def coverage(phraseTable1, phraseTable2):
+# returns percentage of phrases in phraseTable2 that are in phraseTable1	
+def coverageSimple(phraseTable1, phraseTable2):
 	phrases1 = set(phraseTable1.keys())
 	phrases2 = set(phraseTable2.keys())
 	numberOfPhrasesInCommon = len(phrases1.intersection(phrases2))
