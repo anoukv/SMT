2b6eb4c16dc0ef249af98248db13556b40218843
anoukv <anouk.visser@student.uva.nl>
2014-04-28 12:32:17 +0200

    started more complmplicated coverage

--- Ass2/coverage.py
+++ Ass2/coverage.py
@@ -6,3 +6,7 @@ def coverageSimple(phraseTable1, phraseTable2):
 	phrases2 = set(phraseTable2.keys())
 	numberOfPhrasesInCommon = len(phrases1.intersection(phrases2))
 	return numberOfPhrasesInCommon * 100 / float(len(phrases2))
+
+def coverage(phraseTable1, phraseTable1, n=3):
+	coverage = 0
+	return coverage
