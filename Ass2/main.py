ea30acc7564f7c0e77999b6f50baa1283deaa3ad
RemideZ <remi.de.z@gmail.com>
2014-05-04 21:12:43 +0200

    maybe working

--- Ass2/main.py
+++ Ass2/main.py
@@ -1,6 +1,7 @@
 from utils import *
 
 from collections import defaultdict
+from time import time
 
 def extract_phrases_from_sentence(sentence, length=4):
 	"""
@@ -24,37 +25,57 @@ def extract_phrases_from_sentence(sentence, length=4):
 			gram.pop(0)
 		add_gram_to_dic(gram, phrases)
 		index += 1
-
 	return phrases
 
 def get_valid_phrase_pairs(p_e, p_f, allignment):
-	get_index_list = lambda x : [ y[0] for y in x ]
-	get_words_tuple = lambda x : tuple([ y[1] for y in x ])
 	""" 
 		Checks all combinations of p_e and p_f elements for being valid sub-phrases.
 		Function starts with many private declarations. Then computes in double for loop.
 	"""
-	
+	def allignment_is_bidirectional(p_e, p_f, allignment, inv_allignment):
+		""" 
+			Asserts an allignment is bidirectional.
+			This checks the first two conditions in Koehn Chapter 5.2.2 eq. 5.3
+		"""
+		# For each direction
+		for (p1, p2, a) in ((p_e, p_f, allignment), (p_f, p_e, inv_allignment)):
+			# For every word number in sequence1
+			for n in p1:
+				# If it has an allignment
+				if n in a:
+					# For every word number in its' alignment
+					for e in a[n]:
+						# That word has to be in the opposing sentence
+						if not e in p2:
+							return False
+		# Did not find violations.
+		return True
 
+	def set_has_member(s1, s2):
+		""" 
+			Asserts that at least one element in s1 is in s2.
+			This checks the third condition in Koehn Chapter 5.2.2 eq. 5.3 
+		"""
+		for elem in s1:
+			if elem in s2:
+				return True
+		return False
+
+	get_index_list = lambda x : set([ y[0] for y in x ])
+	get_words_tuple = lambda x : tuple([ y[1] for y in x ])
+	
 	inv_allignment = invert_allignment(allignment)
-	print allignment
-	print inv_allignment
 
 	# For every possible pair (e,f) check if the conditions hold that:
 	# 	alligned_has_no_interupts AND allignment_is_bidirectional
 	pairs = []
 	for e in p_e:
-		print "Considering phrase:", e
-		if alligned_has_no_interupts(get_index_list(e), allignment):
-			print "This phrase alligned has no interupts."
+		e_indexes = get_index_list(e)
+		if set_has_member(e_indexes, allignment.keys()):
 			for f in p_f:
-				print "	Combining with", f
-				if allignment_is_bidirectional(get_index_list(e), allignment, inv_allignment):
-					print "		Pair was accepted:", (get_words_tuple(e), get_words_tuple(f))
+				if allignment_is_bidirectional(e_indexes, get_index_list(f), allignment, inv_allignment):
 					pairs.append((get_words_tuple(e), get_words_tuple(f)))
-	print "This pairs:"
-	print pairs
-	print
+	
 	return pairs
 
 def extract_all_phrases(sentence_pairs):
@@ -93,67 +114,29 @@ def extract_all_phrases(sentence_pairs):
 	
 	return (phrases_e, phrases_f, cocs)
 
-def alligned_has_no_interupts(phrase, allignment):
-	""" 
-		Checks if elements in sequence have no interupts
-		(are a continuous phrase) in the alligned language 
-		Does not deal with NULL allignments
-	"""
-	alligned = set()
-	for n in phrase:
-		if n in allignment:
-			for e in allignment[n]:
-				alligned.add(e)
-
-	alligned = sorted(alligned)
-
-	return alligned[-1] == alligned[0] + len(alligned)
-
-def allignment_is_bidirectional(phrase, allignment, inv_allignment):
-	""" 
-		Asserts an allignment is bidirectional.
-		BUG: Does not handle words that were alligned with NULL.
-		Should be easily solved with 'in dict' checking.
-	"""
-	phrase = set(phrase)
-	alligned = set()
-	for p in phrase:
-		if p in allignment:
-			for elem in allignment[p]:
-				alligned.add(elem)
-	inv_alligned = set()
-	for n in alligned:
-		if n in inv_allignment:
-			for elem in inv_allignment[n]:
-				inv_alligned.add(elem)
-
-	# print phrase
-	# print inv_alligned
-	return phrase == inv_alligned
-
 def debug():
 	s_e = [['tot', 'slot', 'is', 'er', 'nog', 'het', 'gebrek', 'aan', 'transparantie', '.']]
 	s_f = [['finally', ',', 'there', 'is', 'the', 'lack', 'of', 'transparency', '.']]
 	allignment = [dict({0: set([0]), 1: set([0, 1]), 2: set([3]), 3: set([2]), 5: set([4]), 6: set([5, 6]), 7: set([5]), 8: set([7]), 9: set([8])})]
 
-	print extract_all_phrases(zip(s_f, s_e, allignment))
-	
+	(phrases_e, phrases_f, cocs) = extract_all_phrases(zip(s_f, s_e, allignment))
+	for e in (phrases_e, phrases_f, cocs):
+		print len(e)
+
 	assert False, "Debug ends here"
 
 if __name__ == "__main__":
-	debug()
 	print "Reading data..."
 	allignment = read_word_allignment_dicts()
 	s_f = read_sentences_from_file("training/p2_training.nl")
 	s_e = read_sentences_from_file("training/p2_training.en")
-	print s_f
-	print s_e
-	assert False
-
 	print "Extracting phrases..."
-	pairs = zip(s_f, s_e, allignment)[:5]
+	start = time()
+	pairs = zip(s_f, s_e, allignment)
 	(phrases_e, phrases_f, cocs) = extract_all_phrases(pairs)
-	print len(phrases_f), len(phrases_e), len(cocs)
+	stop = time()
+	print "Time: ", int(stop - start + 0.5), "seconds"
+	print len(phrases_f), len(phrases_e), len(cocs), len(pairs)
 
 
 
