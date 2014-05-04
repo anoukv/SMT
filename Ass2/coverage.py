eb84b5f6ffbb2a4aa91b6effe281abc40621bdf7
anoukv <anouk.visser@student.uva.nl>
2014-05-04 21:30:15 +0200

    did not commit last time

--- Ass2/coverage.py
+++ Ass2/coverage.py
@@ -8,5 +8,23 @@ def coverageSimple(phraseTable1, phraseTable2):
 	return numberOfPhrasesInCommon * 100 / float(len(phrases2))
 
 def coverage(phraseTable1, phraseTable2, n=3):
+
+	def extractAllWithTerms(phraseTable1, termsSource, termsTarget):
+		result = set()
+		for (source, target) in phraseTable1:
+			if len(termsSource.intersection(set(source))) >  0 and len(termsTarget.intersection(set(target))) > 0:
+				result.add((source, target))
+		return result 
+
 	coverage = 0
+	for phrasePair in prhaseTable2:
+		# check if it is already in there
+		if phrasePair in phraseTable1:
+			coverage += 1
+		else:
+
+
+		# check if we can build it
+
+
 	return coverage
