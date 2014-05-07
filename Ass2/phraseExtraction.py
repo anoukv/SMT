8a3f5f4381e05b8d2c9c5cbc19376fe18eeb5173
RemideZ <remi.de.z@gmail.com>
2014-05-07 21:09:54 +0200

    slapen

--- Ass2/phraseExtraction.py
+++ Ass2/phraseExtraction.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from time import time
+
 from utils import *
 
 def extractAllPhrasesUpToLenght(sentence, n=4):
@@ -6,15 +8,16 @@ def extractAllPhrasesUpToLenght(sentence, n=4):
 	for start in xrange(len(sentence)):
 		for end in xrange(start, min(start+n, len(sentence))):
 			phrases.add(tuple([i for i in xrange(start, end+1)]))
-	return sorted(list(phrases), key = lambda x: x[0])
+	return phrases #sorted(list(phrases), key = lambda x: x[0])
 
 def getText(phrase, sentence):
-	return [sentence[i] for i in phrase]
+	return tuple([sentence[i] for i in phrase])
 
 def prettyPrint(phrasePairs, s_e, s_d):
 	phrasePairs = sorted(phrasePairs, key = lambda x: x[0])
 	for p in phrasePairs:
 		print getText(p[0], s_e), getText(p[1], s_d)
+	print
 	return 0
 
 def checkConsistency(phrases_e, phrases_d, alignmnent):
@@ -34,17 +37,69 @@ def checkConsistency(phrases_e, phrases_d, alignmnent):
 				if word_e in alignmnent:
 					alignment_e = alignment_e.union(alignmnent[word_e])
 					inAlignment_e = True
-			
-			for word_d in phrase_d:
-				if word_d in inverseAlignment:
-					alignment_d = alignment_d.union(inverseAlignment[word_d])
-					inAlignment_d = True
 
+			if inAlignment_e:
+				for word_d in phrase_d:
+					if word_d in inverseAlignment:
+						alignment_d = alignment_d.union(inverseAlignment[word_d])
+						inAlignment_d = True
 
-			if alignment_d.issubset(phrase_e) and alignment_e.issubset(phrase_d) and inAlignment_e and inAlignment_d:
-				phrasePairs.append((phrase_e, phrase_d))
+				if inAlignment_d and alignment_d.issubset(phrase_e) and alignment_e.issubset(phrase_d):
+					phrasePairs.append((phrase_e, phrase_d))
 
 	return phrasePairs
+
+def P_e_given_f(e, f, cocs_f_e):
+	if f in cocs_f_e and e in cocs_f_e[f]:
+		freq_e_f = cocs_f_e[f][e]
+		freq_total = sum( cocs_f_e[f].values() )
+		return freq_e_f / float(freq_total)
+	else:
+		return 0
+
+def P_f_given_e(f, e, cocs_e_f):
+	return P_e_given_f(f, e, cocs_e_f)
+
+def P_joint_e_f(e, f, phrases_f, cocs_f_e):
+	if f in phrases_f:
+		P_of_f = phrases_f[f] / float(sum( phrases_f.values() ))
+		P_of_e_given_f = P_e_given_f(e, f, cocs_f_e)
+		# print "Joining:", P_of_f, "*", P_of_e_given_f
+		return P_of_f * P_of_e_given_f
+	else:
+		return 0
+
+def generate_tables_from_sentences(triplets):
+	def freqcrement(dic, phrases):
+		for phrase in phrases:
+			dic[phrase] += 1
+
+	def coccrement(phrasePairs, dic, reverse=False):
+		if reverse:
+			phrasePairs = [ (x[1], x[0]) for x in phrasePairs ]
+		for (phrase1, phrase2) in phrasePairs:
+			if phrase1 not in dic:
+				dic[phrase1] = defaultdict(int)
+			dic[phrase1][phrase2] += 1
+
+	(freqs_e, freqs_d) = (defaultdict(int), defaultdict(int))
+	(cocs_e_f, cocs_f_e) = (dict(), dict())
+
+	for (s_e, s_d, alignmnentDict) in triplets:
+		phrases_e = extractAllPhrasesUpToLenght(s_e)
+		phrases_d = extractAllPhrasesUpToLenght(s_d)
+
+		freqcrement(freqs_e, [getText(x, s_e) for x in phrases_e])
+		freqcrement(freqs_d, [getText(x, s_d) for x in phrases_d])
+
+		phraseIndexes = checkConsistency(phrases_e, phrases_d, alignmnentDict)
+		# prettyPrint(phraseIndexes, s_e, s_d)
+
+		phrasePairs = [ (getText(x[0], s_e), getText(x[1], s_d)) for x in phraseIndexes ]
+		coccrement(phrasePairs, cocs_e_f, False)
+		coccrement(phrasePairs, cocs_f_e, True)
+
+	return (freqs_e, freqs_d, cocs_e_f, cocs_f_e)
 
 if __name__ == "__main__":
 	
@@ -58,14 +113,29 @@ if __name__ == "__main__":
 		for (x, y) in alignmnent:
 			alignmnentDict[x].add(y)
 	else:
-		
 		print "Reading alignments..."
-		alignmnentDict = read_word_allignment_dicts("heldout/p2_heldout_symal.nlen")[5]
+		alignmnentDicts = read_word_allignment_dicts("heldout/p2_heldout_symal.nlen")
 		print "Reading sentences..."
-		s_e = read_sentences_from_file("heldout/p2_heldout.nl")[5]
-		s_d = read_sentences_from_file("heldout/p2_heldout.en")[5]
+		s_e = read_sentences_from_file("heldout/p2_heldout.nl")
+		s_d = read_sentences_from_file("heldout/p2_heldout.en")
+		print "Extracting..."
+		start = time()
+		triplets = zip(s_e, s_d, alignmnentDicts)
+		(freqs_e, freqs_d, cocs_e_f, cocs_f_e) = generate_tables_from_sentences(triplets)
+		stop = time()
+		print "Time:", int(stop-start), "seconds"
+		for i in xrange(10):
+			(e, f) = (cocs_e_f.keys()[i], cocs_e_f[cocs_e_f.keys()[i]].keys()[0])
+			print "\nPair:", (e, f)
+			print "P(e|f) =", P_e_given_f(e, f, cocs_f_e)
+			print "P(f|e) =", P_f_given_e(f, e, cocs_e_f)
+			print "P(e,f) =", P_joint_e_f(e, f, freqs_d, cocs_f_e)
 
-	phrases_e = extractAllPhrasesUpToLenght(s_e)
-	phrases_d = extractAllPhrasesUpToLenght(s_d)
-	phrasePairs = checkConsistency(phrases_e, phrases_d, alignmnentDict)
-	prettyPrint(phrasePairs, s_e, s_d)
+		print len(freqs_e)
+		print len(freqs_d)
+		print len(cocs_e_f)
+		print len(cocs_f_e)
+
+
+
+
