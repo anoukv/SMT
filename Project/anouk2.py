ccaf33c2c5bac65e73e1ae2ad49ec7070d40e8ca
anoukv <anouk.visser@student.uva.nl>
2014-05-22 11:50:36 +0200

    extracting relative frequency profile

--- Project/anouk2.py
+++ Project/anouk2.py
@@ -2,10 +2,22 @@ from readers import *
 import nltk as nltk
 from nltk.tag.simplify import simplify_wsj_tag
 from collections import defaultdict
+from time import time
+import shelve
 
 def prettyPrint(dictionary):
 	for key in dictionary:
 		print key, dictionary[key]
+
+def normalizeProfile(profile):
+	
+	for key in profile:
+		counts = profile[key]
+		total = float(sum(counts.values()))
+		for key in counts:
+			counts[key] = counts[key] / total
+	
+	return profile
 
 def makeProfile(taggedSentences):
 	profile = dict()
@@ -25,21 +37,47 @@ def makeProfile(taggedSentences):
 				profile[tag] = counting
 	return profile
 
+def extractRelativeFrequencyProfile(corpus):
+	
+	print "Reading corpus..."
+	start = time()
+	pairs = read_sentences(corpus, False)
+	pairs = pairs[:500]
+	sentences = [list(x[0]) for x in pairs]
+	stop = time()
+	print "Reading corpus took", int(stop-start+0.5), "seconds."
+	
+	print "Tagging corpus..."
+	start = time()
+	sentences = [nltk.pos_tag(sen) for sen in sentences]
+	stop = time()
+	print "Tagging corpus took", int(stop-start+0.5), "seconds."
+
+	print "Creating profile..."
+	start = time()
+	profile = makeProfile(sentences)
+	stop = time()
+	print "Creating profile took", int(stop-start+0.5), "seconds."
+	
+	print "Normalizing profile..."
+	start = time()
+	profile = normalizeProfile(profile)
+	stop = time()
+	print "Normalizing profile took", int(stop-start+0.5), "seconds."
+
+	result = shelve.open(corpus + "_rf_profile")
+	result.update(profile)
+	result.close()
+
+	return 1
+
 if __name__ == "__main__":
-	pairsSoftware = read_sentences('software', False)
-	pairsLegal = read_sentences('legal', False)
-	software = [list(x[0]) for x in pairsSoftware]
-	legal = [list(x[0]) for x in pairsLegal]
+	extractRelativeFrequencyProfile('software')
+	extractRelativeFrequencyProfile('legal')
 	
-	# for testing...
-	software = software[0:5]
-	legal = legal[0:5]
-	
-	# tag all sentences in the corpus
-	software = [nltk.pos_tag(sen) for sen in software]
-	legal = [nltk.pos_tag(sen) for sen in legal]
 
 
-	prettyPrint(makeProfile(software))
+	# prettyPrint(profile)
+
 
 
