ab9b8322be249a5ce2957646985011ccbcbf6fa2
anoukv <anouk.visser@student.uva.nl>
2014-05-22 10:55:45 +0200

    make profile

--- Project/anouk2.py
+++ Project/anouk2.py
@@ -7,22 +7,39 @@ def prettyPrint(dictionary):
 	for key in dictionary:
 		print key, dictionary[key]
 
+def makeProfile(taggedSentences):
+	profile = dict()
+	for sentence in taggedSentences:
+		for taggedTuple in sentence:
+			tag = taggedTuple[1]
+			word = taggedTuple[0]
+			if tag in profile:
+				counting = profile[tag]
+				if word in counting:
+					counting[word] += 1
+				else:
+					counting[word] = 1
+			else:
+				counting = dict()
+				counting[word] = 1
+				profile[tag] = counting
+	return profile
+
 if __name__ == "__main__":
 	pairsSoftware = read_sentences('software', False)
 	pairsLegal = read_sentences('legal', False)
 	software = [list(x[0]) for x in pairsSoftware]
 	legal = [list(x[0]) for x in pairsLegal]
+	
+	# for testing...
 	software = software[0:5]
 	legal = legal[0:5]
+	
+	# tag all sentences in the corpus
 	software = [nltk.pos_tag(sen) for sen in software]
 	legal = [nltk.pos_tag(sen) for sen in legal]
-	#print software, legal
 
-	profile = defaultdict(set)
-	for sen in software:
-		for tup in sen:
-			profile[tup[1]].add(tup[0])
 
-	prettyPrint(dict(profile))
+	prettyPrint(makeProfile(software))
 
 
