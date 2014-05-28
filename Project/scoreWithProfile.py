9eff2d8cc836a87a1012fd46654f55ee353733d7
anoukv <anouk.visser@student.uva.nl>
2014-05-28 11:16:01 +0200

    fixed some bugs

--- Project/scoreWithProfile.py
+++ Project/scoreWithProfile.py
@@ -12,7 +12,7 @@ def score(sentence, differences):
 	if score <= 0:
 		score = 0
 	else:
-		score = score / float(sen(sentence))
+		score = score / float(len(sentence))
 
 	return score
 
@@ -26,14 +26,15 @@ def scoreSentences(sentences, differencesEN, differencesES):
 		scoreEN = score(en, differencesEN)
 		scoreES = score(es, differencesES)
 
-		score = (scoreEN + scoreES) / float(2)
+		finalScore = (scoreEN + scoreES) / float(2)
 
-		results.append((bool, score))
+		results.append((bool, finalScore))
 	resultsSorted = sorted(results, key = lambda x: x[1], reverse=True)
 	return resultsSorted
 
 if __name__ == "__main__":	
 	
+	print "Reading difference files"
 	differencesSoftwareEN = shelve.open("profiles/software_difference_profile.en")
 	differencesLegalEN = shelve.open("profiles/legal_difference_profile.en")
 	differencesSoftwareES = shelve.open("profiles/software_difference_profile.es")
@@ -43,10 +44,15 @@ if __name__ == "__main__":
 	if len(differencesSoftwareEN.keys()) == 0 or len(differencesLegalEN.keys()) == 0 or len(differencesSoftwareES.keys()) == 0 or len(differencesLegalES.keys()) == 0 :
 		print "The required files were not yet created."
 	
-	else:	
+	else:
+		print "Reading corpus files"	
 		((mixedLegal, legal_mixed), (mixedSoftware, software_mixed)) = read_datasets(descriminative=True, tagTuples=True)
 
+		print "Computing results"
 		resLegal = scoreSentences(mixedLegal, differencesSoftwareEN, differencesSoftwareES)
 		resSoftware = scoreSentences(mixedSoftware, differencesLegalEN, differencesLegalES)
+		plot_retreival([x[0] for x in resLegal])
+		plot_retreival([x[0] for x in resSoftware])
+		print "Done!"
 
 	
